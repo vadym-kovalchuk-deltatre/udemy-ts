8333cbd2a35136e9281ddf3c8db8d856161daa7c
TeeZ0NE <teez0ne.flyi4@gmail.com>
2020-08-15 14:45:59 +0300

    Return type as function

---
+++ base.ts
@@ -0,0 +1,40 @@
+let person: {
+  name: string;
+  age: number;
+  tags: string[];
+  details: {
+    title: string;
+    desc: string;
+  };
+  tuple: [number, string];
+} = {
+  name: "TeeZ0Ne",
+  age: 41,
+  tags: ["blue", "red", "gray"],
+  details: {
+    title: "",
+    desc: "Some desc",
+  },
+  tuple: [1, "admin"],
+};
+
+for (const tag of person.tags) {
+  console.log(tag.toUpperCase());
+}
+
+enum Role {
+  ADMIN = "admin",
+  USER = "user",
+  CUSTOMER = 1,
+}
+
+function counterWCb(a: number, b: number, cb: (num: number) => number) {
+  let result = a + b;
+  return cb(result);
+}
+
+console.log(
+  counterWCb(1, 2, (result) => {
+    return result * 2;
+  })
+);
