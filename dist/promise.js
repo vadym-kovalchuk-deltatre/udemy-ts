525788592b91f3ed26ca9d1f8bf2f79ea13c8794
TeeZ0NE <teez0ne.flyi4@gmail.com>
2021-04-04 13:47:48 +0300

    upd

--- dist/promise.js
+++ dist/promise.js
@@ -1,17 +1,4 @@
 "use strict";
-const getGeoPos = () => {
-    const promise = new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition((success) => {
-            resolve(success);
-        }, (error) => {
-            reject(error);
-        });
-    });
-    return promise;
-};
-getGeoPos()
-    .then((successLog) => console.log(successLog))
-    .catch((errorLog) => console.log(errorLog.message));
 const setTimer = (duration, isTimer) => {
     const promise = new Promise((resolve, reject) => {
         if (isTimer) {
@@ -25,5 +12,36 @@ const setTimer = (duration, isTimer) => {
     });
     return promise;
 };
-setTimer(1000, false).then((log) => console.log(log), (errLog) => console.log(errLog));
-setTimer(1000, true).then((log) => console.log(log), (elseLog) => console.log(elseLog));
+const setTimerAsync = (duration, timerNum) => {
+    const promise = new Promise((success, reject) => {
+        if (timerNum > 2) {
+            setTimeout(() => success(timerNum.toString()), duration);
+        }
+        else {
+            reject("Less then true");
+        }
+    });
+    return promise;
+};
+const twoTimers = async () => {
+    try {
+        const timer1 = await setTimerAsync(400, 3);
+        console.log("timer:" + timer1);
+        const timer2 = await setTimer(1000, true);
+        console.log("timer2: " + timer2);
+    }
+    catch (error) {
+        console.log(error);
+    }
+    console.log("After try catch");
+};
+Promise.race([setTimerAsync(4444, 3), setTimerAsync(2000, 1)])
+    .then((result) => console.log(result))
+    .catch((result) => console.log(result));
+Promise.all([setTimerAsync(100, 3), setTimerAsync(200, 4)])
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error));
+Promise.allSettled([
+    setTimerAsync(100, 2),
+    setTimerAsync(200, 4),
+]).then((result) => console.log(result));
