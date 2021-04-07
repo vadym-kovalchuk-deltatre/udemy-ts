e2a4a61e592f8e8507ca0c931e8c8caa4a358511
Vadym Kovalchuk <Vadym_Kovalchuk1@massive.co>
2021-04-07 22:01:34 +0300

    Change to getter and setter in interface

--- src/interfaces.dev.ts
+++ src/interfaces.dev.ts
@@ -1,52 +1,52 @@
 // Or replace interface to type, it'll be the same //
 // type PersonInterface = {} //
 interface PersonInterface {
-  firstName: string;
-  readonly email: string;
-  phones: string[];
-  address?: string;
-
-  addPhoneNumber(number: string): void;
-  getPhoneNumbers(): string[];
-  getInfo(): string;
+	firstName: string;
+	readonly email: string;
+	phones: string[];
+	notes?: string[];
+	addPhoneNumber(number: string): void;
+	getPhoneNumbers(): string[];
+	getInfo(): string;
 }
 
 interface EmailGetter extends PersonInterface {
-  getEmail(): string;
-  getAddress?(): string;
+	getEmail(): string;
+	setNotes?(): void;
+	getNotes?(): string[];
 }
 
 class Person implements EmailGetter {
-  readonly phones: string[] = [];
-  constructor(
-    public firstName: string,
-    readonly email: string,
-    public address: string = "Default address"
-  ) {}
+	readonly phones: string[] = [];
+	private address: string = "Default address";
+	constructor(public firstName: string, readonly email: string) {}
 
-  addPhoneNumber(phone: string): void {
-    this.phones.push(phone);
-  }
+	addPhoneNumber(phone: string): void {
+		this.phones.push(phone);
+	}
 
-  getPhoneNumbers() {
-    return this.phones;
-  }
+	set personAddress(address: string) {
+		this.address = address;
+	}
+	getPhoneNumbers() {
+		return this.phones;
+	}
 
-  getInfo(): string {
-    return `
+	getInfo() {
+		return `
     Person name is: ${this.firstName},
     email: ${this.email},
     phones: ${this.phones},
-    address: ${this.address}`;
-  }
+    address: ${this.personAddress}`;
+	}
 
-  getEmail(): string {
-    return this.email;
-  }
+	getEmail() {
+		return this.email;
+	}
 
-  getAddress(): string {
-    return this.address;
-  }
+	get personAddress() {
+		return this.address;
+	}
 }
 
 // ~~~~~~~~~~~ Create one Person ~~~~~~~~~~ //
@@ -54,23 +54,24 @@ let person1 = new Person("Teez0ne", "teez0ne@");
 console.log(person1.getInfo());
 person1.addPhoneNumber("(270) 307-7745");
 person1.addPhoneNumber("(419) 471-3518");
+person1.personAddress = "New Hampshire";
 console.log(person1.getInfo());
 console.log("User email method: " + person1.getEmail());
 
 // ~~~~ Create Person 2 from Interface ~~~ //
 let person2: PersonInterface = {
-  firstName: "Margaret Clarke",
-  email: "hannu@lu.sd",
-  phones: ["(641) 663-7883"],
-  addPhoneNumber(number: string): void {
-    this.phones.push(number);
-  },
-  getPhoneNumbers(): string[] {
-    return this.phones;
-  },
-  getInfo() {
-    return "" + this.firstName + " email: " + this.email;
-  },
+	firstName: "Margaret Clarke",
+	email: "hannu@lu.sd",
+	phones: ["(641) 663-7883"],
+	addPhoneNumber(number: string) {
+		this.phones.push(number);
+	},
+	getPhoneNumbers() {
+		return this.phones;
+	},
+	getInfo() {
+		return "" + this.firstName + " email: " + this.email;
+	},
 };
 
 console.log(person2.getPhoneNumbers());
