4977b946e09dd095f13fef3136b00769875abfee
TeeZ0NE <teez0ne.flyi4@gmail.com>
2021-04-19 22:16:58 +0300

    updt interf

--- src/interfaces.dev.ts
+++ src/interfaces.dev.ts
@@ -4,47 +4,31 @@ interface PersonInterface {
 	firstName: string;
 	readonly email: string;
 	phones: string[];
-<<<<<<< HEAD
 	address?: string;
 
 	addPhoneNumber(phoneNumber: string): void;
 	setAddres?(addres: string): void;
-=======
-	notes?: string[];
-	addPhoneNumber(number: string): void;
->>>>>>> e2a4a61e592f8e8507ca0c931e8c8caa4a358511
 	getPhoneNumbers(): string[];
 	getInfo(): string;
 }
 
 interface EmailGetter extends PersonInterface {
 	getEmail(): string;
-<<<<<<< HEAD
 	getAddress?(): string;
-=======
-	setNotes?(): void;
-	getNotes?(): string[];
->>>>>>> e2a4a61e592f8e8507ca0c931e8c8caa4a358511
 }
 
 class Person implements EmailGetter {
 	readonly phones: string[] = [];
-<<<<<<< HEAD
 	constructor(
 		public firstName: string,
 		readonly email: string,
 		public address: string = "Default address"
 	) {}
-=======
-	private address: string = "Default address";
-	constructor(public firstName: string, readonly email: string) {}
->>>>>>> e2a4a61e592f8e8507ca0c931e8c8caa4a358511
 
 	addPhoneNumber(phone: string): void {
 		this.phones.push(phone);
 	}
 
-<<<<<<< HEAD
 	setAddres(addres: string): void {
 		this.address = addres;
 	}
@@ -54,21 +38,10 @@ class Person implements EmailGetter {
 	}
 
 	getInfo(): string {
-=======
-	set personAddress(address: string) {
-		this.address = address;
-	}
-	getPhoneNumbers() {
-		return this.phones;
-	}
-
-	getInfo() {
->>>>>>> e2a4a61e592f8e8507ca0c931e8c8caa4a358511
 		return `
     Person name is: ${this.firstName},
     email: ${this.email},
     phones: ${this.phones},
-<<<<<<< HEAD
     address: ${this.address}`;
 	}
 
@@ -77,16 +50,6 @@ class Person implements EmailGetter {
 	}
 
 	getAddress(): string {
-=======
-    address: ${this.personAddress}`;
-	}
-
-	getEmail() {
-		return this.email;
-	}
-
-	get personAddress() {
->>>>>>> e2a4a61e592f8e8507ca0c931e8c8caa4a358511
 		return this.address;
 	}
 }
@@ -96,11 +59,7 @@ let person1 = new Person("Teez0ne", "teez0ne@");
 console.log(person1.getInfo());
 person1.addPhoneNumber("(270) 307-7745");
 person1.addPhoneNumber("(419) 471-3518");
-<<<<<<< HEAD
 person1.setAddres("655 Hanuwi Pass");
-=======
-person1.personAddress = "New Hampshire";
->>>>>>> e2a4a61e592f8e8507ca0c931e8c8caa4a358511
 console.log(person1.getInfo());
 console.log("User email method: " + person1.getEmail());
 
@@ -109,7 +68,6 @@ let person2: PersonInterface = {
 	firstName: "Margaret Clarke",
 	email: "hannu@lu.sd",
 	phones: ["(641) 663-7883"],
-<<<<<<< HEAD
 	address: "New Address",
 	addPhoneNumber(phoneNumber: string): void {
 		this.phones.push(phoneNumber);
@@ -119,16 +77,6 @@ let person2: PersonInterface = {
 	},
 	getInfo() {
 		return "" + this.firstName + " email: " + this.email + this.address;
-=======
-	addPhoneNumber(number: string) {
-		this.phones.push(number);
-	},
-	getPhoneNumbers() {
-		return this.phones;
-	},
-	getInfo() {
-		return "" + this.firstName + " email: " + this.email;
->>>>>>> e2a4a61e592f8e8507ca0c931e8c8caa4a358511
 	},
 };
 
