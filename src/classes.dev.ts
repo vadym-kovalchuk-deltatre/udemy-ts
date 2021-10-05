dead78f10b3eb9143496002c7dfa8364868b6d3c
Vadym Kovalchuk <Vadym_Kovalchuk1@massive.co>
2021-10-05 09:43:26 +0300

    Upd node

--- src/classes.dev.ts
+++ src/classes.dev.ts
@@ -1,55 +1,55 @@
 abstract class Department {
-  protected employees: string[] = [];
+	protected employees: string[] = [];
 
-  constructor(public readonly id: number, public departmentName: string) {}
+	constructor(public readonly id: number, public departmentName: string) {}
 
-  describe(this: Department) {
-    console.log(`dep id: ${this.id} dep name: ${this.departmentName}`);
-  }
+	describe(this: Department) {
+		console.log(`dep id: ${this.id} dep name: ${this.departmentName}`);
+	}
 
-  addEmployee(name: string) {
-    this.employees.push(name);
-  }
+	addEmployee(name: string) {
+		this.employees.push(name);
+	}
 
-  abstract printEmployees(): void | never;
+	abstract printEmployees(): void | never;
 }
 
 class AccountDepartment extends Department {
-  constructor(public readonly id: number, public nameDepartment: string) {
-    super(id, nameDepartment);
-  }
-  printEmployees() {
-    console.log("Account department employess: ", this.employees);
-  }
+	constructor(public readonly id: number, public nameDepartment: string) {
+		super(id, nameDepartment);
+	}
+	printEmployees() {
+		console.log("Account department employess: ", this.employees);
+	}
 }
 class ITDepartment extends Department {
-  private serverNames: string[] = [];
-  static defaultName = "IT dep";
-  private static instance: ITDepartment;
+	private serverNames: string[] = [];
+	static defaultName = "IT dep";
+	private static instance: ITDepartment;
 
-  private constructor(public readonly id: number) {
-    super(id, "IT");
-  }
+	private constructor(public readonly id: number) {
+		super(id, "IT");
+	}
 
-  get availableServerNames() {
-    return this.serverNames;
-  }
+	get availableServerNames() {
+		return this.serverNames;
+	}
 
-  set availableServerName(serverName: string) {
-    this.serverNames.push(serverName);
-  }
+	set availableServerName(serverName: string) {
+		this.serverNames.push(serverName);
+	}
 
-  printEmployees(): void | never {
-    if (this.employees.length > 4) throw new Error("There're lot of employees");
-    console.log(`${this.departmentName} employees: ${this.employees}`);
-  }
+	printEmployees(): void | never {
+		if (this.employees.length > 4) throw new Error("There're lot of employees");
+		console.log(`${this.departmentName} employees: ${this.employees}`);
+	}
 
-  static getInstance() {
-    if (!ITDepartment.instance) {
-      this.instance = new ITDepartment(3);
-    }
-    return this.instance;
-  }
+	static getInstance() {
+		if (!ITDepartment.instance) {
+			this.instance = new ITDepartment(3);
+		}
+		return this.instance;
+	}
 }
 //  Simple accounting department  //
 let accountingDepartment = new AccountDepartment(1, "accounting");
@@ -58,10 +58,10 @@ accountingDepartment.addEmployee("TeeZ0NE");
 accountingDepartment.printEmployees();
 
 let copyAccountingDepartment = {
-  id: 2,
-  name: "Copy Accounting",
-  describe: accountingDepartment.describe,
-  employees: [],
+	id: 2,
+	name: "Copy Accounting",
+	describe: accountingDepartment.describe,
+	employees: [],
 };
 console.log("copyAccountingDepartment :>> ", copyAccountingDepartment);
 // ----- IT Department ---- //
@@ -73,5 +73,6 @@ itDepartment.addEmployee("Bob");
 itDepartment.printEmployees();
 console.log("servers", itDepartment.availableServerNames);
 itDepartment.availableServerName = "aws";
+itDepartment.availableServerName = "gDrive";
 console.log("servers " + itDepartment.availableServerNames);
 console.log(ITDepartment.defaultName);
